f9be74fa4b1bdbfc62e9e74ba9d9003f01f1a9c5
Seungjae Park <astro.psj@gmail.com>
2021-07-24 18:47:41 +0900

    Lint code with Clang Format

--- src/stateful.cpp
+++ src/stateful.cpp
@@ -1,59 +1,64 @@
 #include <rapidcheck.h>
 #include <rapidcheck/state.h>
 
-using namespace rc;
-
 class Counter {
 public:
-  void inc() { m_value++; }
-
-  void dec() {
-    assert(m_value > 0);
-    // Broken!
-    if (m_value != 10) {
-      m_value--;
+    void inc() {
+        m_value++;
     }
-  }
 
-  int get() { return m_value; }
+    void dec() {
+        assert(m_value > 0);
+        // Broken!
+        if (m_value != 10) {
+            m_value--;
+        }
+    }
+
+    int get() {
+        return m_value;
+    }
 
 private:
-  int m_value = 0;
+    int m_value = 0;
 };
 
 struct CounterModel {
-  int value = 0;
+    int value = 0;
 };
 
-struct Inc : public state::Command<CounterModel, Counter> {
-  void apply(CounterModel &s0) const override { s0.value++; }
+struct Inc : public rc::state::Command<CounterModel, Counter> {
+    void apply(CounterModel &s0) const override {
+        s0.value++;
+    }
 
-  void run(const CounterModel &s0, Counter &counter) const override {
-    counter.inc();
-    RC_ASSERT(counter.get() == (s0.value + 1));
-  }
+    void run(const CounterModel &s0, Counter &counter) const override {
+        counter.inc();
+        RC_ASSERT(counter.get() == (s0.value + 1));
+    }
 };
 
-struct Dec : public state::Command<CounterModel, Counter> {
-  void checkPreconditions(const CounterModel &s0) const override {
-    RC_PRE(s0.value > 0);
-  }
+struct Dec : public rc::state::Command<CounterModel, Counter> {
+    void checkPreconditions(const CounterModel &s0) const override {
+        RC_PRE(s0.value > 0);
+    }
 
-  void apply(CounterModel &s0) const override { s0.value--; }
+    void apply(CounterModel &s0) const override {
+        s0.value--;
+    }
 
-  void run(const CounterModel &state, Counter &counter) const override {
-    counter.dec();
-    RC_ASSERT(counter.get() == (state.value - 1));
-  }
+    void run(const CounterModel &state, Counter &counter) const override {
+        counter.dec();
+        RC_ASSERT(counter.get() == (state.value - 1));
+    }
 };
 
 int main() {
-  check([] {
-    CounterModel state;
-    Counter sut;
-    state::check(state, sut, state::gen::execOneOfWithArgs<Inc, Dec>());
-  });
+    rc::check([] {
+        CounterModel state;
+        Counter sut;
+        rc::state::check(state, sut, rc::state::gen::execOneOfWithArgs<Inc, Dec>());
+    });
 
-  return 0;
+    return 0;
 }
-
--- src/stateless.cpp
+++ src/stateless.cpp
@@ -1,17 +1,14 @@
+#include <algorithm>
+#include <vector>
 #include <rapidcheck.h>
 
-#include <vector>
-#include <algorithm>
-
 int main() {
-  rc::check("double reversal yields the original value",
-            [](const std::vector<int> &l0) {
-              auto l1 = l0;
-              std::reverse(begin(l1), end(l1));
-              std::reverse(begin(l1), end(l1));
-              RC_ASSERT(l0 == l1);
-            });
-
-  return 0;
+    rc::check("double reversal yields the original value",
+              [](const std::vector<int> &l0) {
+                  auto l1 = l0;
+                  std::reverse(begin(l1), end(l1));
+                  std::reverse(begin(l1), end(l1));
+                  RC_ASSERT(l0 == l1);
+              });
+    return 0;
 }
-
