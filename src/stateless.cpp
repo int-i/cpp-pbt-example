2e2b826ded9984103d5efc2403a9c363e3bc62e3
Seungjae Park <astro.psj@gmail.com>
2021-07-25 14:45:14 +0900

    Fix wrong property

--- src/stateless.cpp
+++ src/stateless.cpp
@@ -12,10 +12,10 @@ int main() {
               });
     rc::check("should always contain its substrings",
               [](const std::string &s0, const std::string &s1, const std::string &s2) {
-                  if (s1.empty()) {
-                      RC_ASSERT(string_search(s0 + s1 + s2, s1) == 0);
-                  } else {
-                      RC_ASSERT(string_search(s0 + s1 + s2, s1) == s0.length());
+                  const std::string s = s0 + s1 + s2;
+                  int idx = string_search(s, s1);
+                  for (int i = 0; i < s1.length(); i++) {
+                      RC_ASSERT(s1[i] == s[idx++]);
                   }
               });
     return 0;
